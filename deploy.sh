85b06393b6639c0dc4bf60b95d19d5c31d111b55
Rainer Gevers <rainer@fastcomm.com>
2020-02-22 12:25:11 +0200

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,16 @@
-docker build -t rainerza/multi-client -f ./client/Dockerfile ./client
-docker build -t rainerza/multi-server -f ./server/Dockerfile ./server
-docker build -t rainerza/multi-worker -f ./worker/Dockerfile ./worker
+docker build -t rainerza/multi-client:$SHA -t rainerza/multi-client:latest -f ./client/Dockerfile ./client
+docker build -t rainerza/multi-server:$SHA -t rainerza/multi-server:latest -f ./server/Dockerfile ./server
+docker build -t rainerza/multi-worker:$SHA -t rainerza/multi-worker:latest -f ./worker/Dockerfile ./worker
+
+docker push rainerza/multi-client:$SHA
+docker push rainerza/multi-server:$SHA
+docker push rainerza/multi-worker:$SHA 
+
+docker push rainerza/multi-client:latest
+docker push rainerza/multi-server:latest
+docker push rainerza/multi-worker:latest
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=rainerza/multi-server:$SHA
+kubectl set image deployments/client-deployment server=rainerza/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=rainerza/multi-worker:$SHA
